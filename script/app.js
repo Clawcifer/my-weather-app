a1696c7925b2cd0d3a3e53eb0a36549ac9729c29
Clawcifer <93573227+Clawcifer@users.noreply.github.com>
2021-12-28 14:56:21 +0100

    Removed the point in the weather forecast
     so that it aligns with the rest

--- script/app.js
+++ script/app.js
@@ -43,7 +43,7 @@ function formatDate(now) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
