5a4759c041894f6df5dac61806e4a52f679ca2de
Clawcifer <93573227+Clawcifer@users.noreply.github.com>
2022-01-08 18:03:53 +0100

    Changed the CSS of the Dark Mode

---
+++ script/darkmode.js
@@ -0,0 +1,144 @@
+function formatDate(now) {
+  let currentDate = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = now.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  let monthIndex = now.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "Mai",
+    "June",
+    "Juli",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[monthIndex];
+
+  return `${day} | ${month} ${currentDate}. | ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+                <div class="col-2">
+                <div class="weather-forecast-date">
+                  ${formatDay(forecastDay.dt)}
+                </div>
+<img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" 
+alt="" 
+width="42">
+<div class="weather-forecast-temperature">
+  <span class="weather-forecast-max">${Math.round(forecastDay.temp.max)}°</span>
+  <span class="weather-forecast-min">${Math.round(forecastDay.temp.min)}°</span>
+</div>
+                </div>
+              `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c37526e7df4d92dc9f8f999d7f534bb7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+let now = new Date();
+let dateElement = document.querySelector("h2");
+dateElement.innerHTML = formatDate(now);
+
+function showWeatherConditions(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#converted-temperature").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+  document
+    .querySelector("#icon")
+    .setAttribute("src", `images/${response.data.weather[0].icon}.svg`);
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function searchCity(city) {
+  let apiKey = "c37526e7df4d92dc9f8f999d7f534bb7";
+  let endPoint = "https://api.openweathermap.org";
+  let units = "metric";
+  let apiUrl = `${endPoint}/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeatherConditions);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", showCity);
+
+function findCurrentPosition(position) {
+  let apiKey = "c37526e7df4d92dc9f8f999d7f534bb7";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeatherConditions);
+}
+function showPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findCurrentPosition);
+}
+let gpsButton = document.querySelector("#geolocation");
+gpsButton.addEventListener("click", showPosition);
+
+searchCity("Colmar");
